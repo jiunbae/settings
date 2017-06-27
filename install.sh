c6b00405980bd84fea0123bc9b4860536e1868f4
MaybeS <maytryark@gmail.com>
2017-06-27 02:48:13 +0000

    Update zsh autosuggestion color

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# init submodules
+git submodule init
+git submoudle update
+
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/
@@ -6,8 +10,10 @@ vim +PluginInstall +qall
 # install zsh
 sudo apt-get install zsh -y
 
+# install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cp .zshrc ~/
 cp -r .zsh ~/
 
-
+# install config omz
+cp zsh-autosuggestions.zsh ~/.zsh/zsh-autosuggestions
