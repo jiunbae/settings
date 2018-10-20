aeba551d87bf5ed81c0e08301aabb53b4af87d46
MaybeS <maytryar@gmail.com>
2018-10-21 01:38:49 +0900

    Update

--- .vimrc
+++ .vimrc
@@ -20,14 +20,8 @@ hi CursorLine ctermbg=14 ctermfg=16 cterm=bold guibg=Grey40
 set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 	Plugin 'gmarik/Vundle.vim'
-	Plugin 'Syntastic'
-	Plugin 'Shougo/neocomplete'
-	Plugin 'Shougo/neosnippet'
-	Plugin 'Shougo/neosnippet-snippets'
-	Plugin 'vim-addon-manager'
-	Bundle 'davidhalter/jedi-vim'
 	Bundle 'Lokaltog/powerline', {'rtp': 'powerline/bindings/vim/'}
 call vundle#end()
 	    
-set guifont=DejaVu\ Sans\ Mono\ for\ Powerline\ 9
 set laststatus=2
+
--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# install git, wget
+sudo apt install git wget -y
+
+# install htop, bat
+sudo apt install htop bat -y
+
 # init submodules
 git submodule init
 git submodule update
@@ -8,7 +14,7 @@ cp .vimrc ~/
 vim +PluginInstall +qall
 
 # install zsh
-sudo apt-get install zsh -y
+sudo apt install zsh -y
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -17,3 +23,4 @@ cp -r .zsh ~/
 
 # install config omz
 cp zsh-autosuggestions.zsh ~/.zsh/zsh-autosuggestions
+
