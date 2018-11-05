de024bb99ee20d1e5d0889972d2aca1341fcc821
jiun.bae <jiun.bae@webtoonscorp.com>
2018-11-05 11:18:17 +0900

    Update change mirror to kakao

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
+# change mirror to kakao
+
+cp /etc/apt/sources.list /etc/apt/sources.list.org
+sed -e 's/\(us.\)\?archive.ubuntu.com/mirror.kakao.com/g' -e 's/security.ubuntu.com/mirror.kakao.com/g' < /etc/apt/sources.list.org > /etc/apt/sources.list
+apt update
+
 # install git, wget
-sudo apt install git wget -y
+apt install git wget -y
 
 # install htop, bat
-sudo apt install htop -y
+apt install htop -y
 
 # init submodules
 git submodule init
@@ -14,7 +20,7 @@ cp .vimrc ~/
 vim +PluginInstall +qall
 
 # install zsh
-sudo apt install zsh -y
+apt install zsh -y
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -23,4 +29,3 @@ cp -r .zsh ~/
 
 # install config omz
 cp zsh-autosuggestions.zsh ~/.zsh/zsh-autosuggestions
-
