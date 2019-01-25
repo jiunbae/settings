8d97ff4bb6c30262de50965f7c5fb63c5e653683
MaybeS <maytryark@gmail.com>
2019-01-25 02:53:40 +0000

    Update install locales

--- .zshrc
+++ .zshrc
@@ -29,8 +29,8 @@ prompt_context() {
   fi
 }
 
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 
 # alias
 alias top=htop
--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ cp /etc/apt/sources.list /etc/apt/sources.list.org
 sed -e 's/\(us.\)\?archive.ubuntu.com/mirror.kakao.com/g' -e 's/security.ubuntu.com/mirror.kakao.com/g' < /etc/apt/sources.list.org > /etc/apt/sources.list
 apt update
 
+# install locale for en_US
+apt install locales -y
+locale-gen en_US.UTF-8
+
 # install default packages
 apt install wget vim zsh -y
 
