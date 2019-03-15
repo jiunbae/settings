8d909e0b3524598977dd8d342cf2b7e8c8ee5842
Maydev <alice.maydev@gmail.com>
2019-03-15 12:01:33 +0900

    Update zsh plugins and pip mirror

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 # change apt mirror to kakao
 cp /etc/apt/sources.list /etc/apt/sources.list.org
 sed -e 's/\(us.\)\?archive.ubuntu.com/mirror.kakao.com/g' -e 's/security.ubuntu.com/mirror.kakao.com/g' < /etc/apt/sources.list.org > /etc/apt/sources.list
-apt update
+sudo apt update
+
+# change pip mirror to kakao
+mkdir -p ~/.pip
+cp pip.conf ~/.pip
 
 # install locale for en_US
-apt install locales -y
+sudo apt install locales -y
 locale-gen en_US.UTF-8
 
 # install default packages
-apt install wget vim zsh -y
-
-# init submodules
-git submodule init
-git submodule update
+sudo apt install wget vim zsh -y
 
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -21,8 +21,9 @@ vim +PluginInstall +qall
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-cp .zshrc ~/
-cp -r .zsh ~/
 
-# install config omz
-cp zsh-autosuggestions.zsh ~/.zsh/zsh-autosuggestions
+# install omz plugins
+git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+cp .zshrc ~/
