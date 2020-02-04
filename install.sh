889f6e8e33b5ce08ad55f264320ef33bef9478b5
jiun, bae <maytryark@gmail.com>
2020-02-04 13:51:07 +0900

    Update git crlf config

--- install.sh
+++ install.sh
@@ -213,6 +213,10 @@ gcc() {
     $SUDOPREFIX update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90 --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9
 }
 
+git() {
+    $GIT config --global core.autocrlf true
+}
+
 ########################################
 # Check requirements
 if [[ ! $(command -v whiptail) ]]; then
