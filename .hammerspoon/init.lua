23ac95c9d316328892ea71ca257521252629dcc1
Jiun Bae <jiunbae.623@gmail.com>
2025-03-25 17:56:57 +0900

    Update hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,26 +4,17 @@ local application = require "hs.application"
 function moveMouseToScreen(screenIndex)
   local screens = hs.screen.allScreens()
   if #screens >= screenIndex then
-      local targetScreen = screens[screenIndex]
-      local pt = hs.geometry.rectMidPoint(targetScreen:fullFrame())
-      hs.mouse.absolutePosition(pt)
-      
-      local space = hs.spaces.activeSpaceOnScreen(targetScreen)
-      if not space then
-          print("⚠️ No active space found for screen " .. screenIndex)
-          return
-      end
+    local targetScreen = screens[screenIndex]
+    local pt = hs.geometry.rectMidPoint(targetScreen:fullFrame())
+    hs.mouse.absolutePosition(pt)
 
-      local visibleWindows = hs.window.filter.new()
-        :setOverrideFilter({visible = true, allowScreens = {targetScreen:id()}})
-        :getWindows()
-
-      if #visibleWindows > 0 then
-          visibleWindows[1]:focus()
-      else
-          print("⚠️ No visible windows found in active space.")
-          return
+    local orderedWindows = hs.window.orderedWindows()
+    for _, window in ipairs(orderedWindows) do
+      if window:screen():id() == targetScreen:id() and window:title() ~= "" then
+        window:focus()
+        break
       end
+    end
   end
 end
 
@@ -37,6 +28,47 @@ function moveWindowToScreen(screenIndex)
   end
 end
 
+--- Function to rotate window focus forward or backward within the same screen and space
+function rotateWindowFocus(direction)
+  local focusedWindow = hs.window.focusedWindow()
+  if not focusedWindow then return end
+  
+  local focusedScreen = focusedWindow:screen()
+  local visibleWindows = hs.window.visibleWindows()
+  
+  local firstVisibleWindows = nil
+  local currentVisibleWindows = nil
+  local previousVisibleWindows = nil
+  local isWindowFocused = false
+
+  for i, window in ipairs(visibleWindows) do
+    if window:screen():id() == focusedScreen:id() and window:title() ~= "" then
+      if firstVisibleWindows == nil then
+        firstVisibleWindows = window
+      end
+      currentVisibleWindows = window
+      
+      if direction == "forward" and previousVisibleWindows == focusedWindow then
+        window:focus()
+        isWindowFocused = true
+        break
+      elseif direction == "backward" and window == focusedWindow and previousVisibleWindows ~= nil then
+        previousVisibleWindows:focus()
+        isWindowFocused = true
+        break
+      end
+      previousVisibleWindows = window
+    end
+  end
+  if not isWindowFocused then
+    if direction == "forward" then
+      firstVisibleWindows:focus()
+    elseif direction == "backward" then
+      currentVisibleWindows:focus()
+    end
+  end
+end
+
 -- Hotkeys to switch mouse focus to the first or second screen
 hs.hotkey.bind({"alt", "shift"}, "i", function() moveMouseToScreen(1) end)
 hs.hotkey.bind({"alt", "shift"}, "u", function() moveMouseToScreen(2) end)
@@ -44,3 +76,7 @@ hs.hotkey.bind({"alt", "shift"}, "u", function() moveMouseToScreen(2) end)
 -- Hotkeys to switch window to the first or second screen
 hs.hotkey.bind({"ctrl", "alt", "shift"}, "i", function() moveWindowToScreen(1) end)
 hs.hotkey.bind({"ctrl", "alt", "shift"}, "u", function() moveWindowToScreen(2) end)
+
+-- Hotkey to cycle window focus
+hs.hotkey.bind({"alt", "shift"}, "j", function() rotateWindowFocus("backward") end)
+hs.hotkey.bind({"alt", "shift"}, "k", function() rotateWindowFocus("forward") end)
